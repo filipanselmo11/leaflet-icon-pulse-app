d0289c6ad0d3ed5c32ad5d32e46290c8a6825305
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-29 13:08:56 -0400

    Add icon delete config

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -17,6 +17,13 @@ import "@ansur/leaflet-pulse-icon/dist/L.Icon.Pulse.css";
 
 const L = window["L"];
 
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
 export default {
   name: "MapComponent",
   data() {
